871e05c9daec439430c1e65fa7e0bd1e43480d91
hetyey-b <hetyey.botond@gmail.com>
2023-09-30 11:31:01 +0200

    exported values

--- tilemap/water_level.gd
+++ tilemap/water_level.gd
@@ -10,8 +10,8 @@ const ISLAND_LVL_COORDS = [
 	]
 const WATER_TILE_COORDS = Vector2i(0,1)
 
-const WATER_RISE_TICK = 5
-const TILES_PER_TICK = 20
+@export var WATER_RISE_TICK = 5
+@export var TILES_PER_TICK = 20
 
 var time_passed = 0
 
@@ -34,24 +34,21 @@ func _process(delta):
 func raise_water_lvl():
 	var rng = RandomNumberGenerator.new()
 	var player_pos = get_node("../Player").position
-	#print("palyerpos:")
-	#print(player_pos)
+
 	for i in range(0,TILES_PER_TICK):
 		var tiles = get_used_cells_by_id(0,0,ISLAND_LVL_COORDS[current_water_lvl])
 		var rnd_ind = rng.randf_range(0,len(tiles))
 		var remove_coord = tiles.pop_at(rnd_ind)
 		var global_coord = to_global(map_to_local(remove_coord))
-		#print("remove coord:")
-		#print(remove_coord)
-		#print("global:")
-		#print(global_coord)
-		#print("====================")
+
 		
 		if abs(player_pos[0] - global_coord[0]) <= 8 && abs(player_pos[1] - global_coord[1]) <= 8:
-			print("GAME OVER")
+			game_over()
 			
 		set_cell(0,remove_coord,0,WATER_TILE_COORDS)
 		if len(tiles) == 0 :
 			current_water_lvl += 1
 	pass
 
+func game_over():
+	print("GAME OVER")
