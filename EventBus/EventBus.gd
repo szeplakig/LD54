cf4b365ac495a38fdbe3236b6d3f5facd840a52c
hetyey-b <hetyey.botond@gmail.com>
2023-09-30 18:59:11 +0200

    Bugfix: win screen blocked clicks

--- EventBus/EventBus.gd
+++ EventBus/EventBus.gd
@@ -6,10 +6,12 @@ signal PlayerWin(score: int);
 func player_damaged(max_health: float, current_health: float, damage_amount: float):
 	PlayerDamaged.emit(max_health, current_health, damage_amount)
 	if current_health == 0:
-		game_over()
+		player_win(42)
+		#game_over()
 
 func game_over():
 	get_tree().change_scene_to_file("res://game_over.tscn")
 
 func player_win(score: int):
 	PlayerWin.emit(score)
+	%WinScreen.visible = true
