412dfce6a0cf3c6ca160283cdd69b05962bedf6c
steiner2001 <steiner.akos.2001@gmail.com>
2023-09-30 17:34:04 +0200

    [LD-54] ?

--- object/ship/ship_frame.gd
+++ object/ship/ship_frame.gd
@@ -3,10 +3,12 @@ extends TextureButton
 var phase = 1
 
 func _on_pressed():
-	phase += 1
-	if phase < 8:
-		texture_normal = ImageTexture.create_from_image(Image.load_from_file("res://object/ship/assets/frame"+str(phase)+".png"))
-	elif phase == 8:
-		texture_normal = ImageTexture.create_from_image(Image.load_from_file("res://object/ship/assets/built.png"))
-	else:
-		pass
+	if (not get_node("../Player").currentItem == null) and "PlankItem" in get_node("../Player").currentItem.name:
+		get_node("../Player").currentItem = null
+		phase += 1
+		if phase < 8:
+			texture_normal = ImageTexture.create_from_image(Image.load_from_file("res://object/ship/assets/frame"+str(phase)+".png"))
+		elif phase == 8:
+			texture_normal = ImageTexture.create_from_image(Image.load_from_file("res://object/ship/assets/built.png"))
+		else:
+			pass
