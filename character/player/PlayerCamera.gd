e8dd10ed849777a677e21a82687ee2ea886cad42
steiner2001 <steiner.akos.2001@gmail.com>
2023-09-30 14:59:03 +0200

    [LD-54] feat: chopping trees

--- character/player/PlayerCamera.gd
+++ character/player/PlayerCamera.gd
@@ -1,6 +1,15 @@
 extends Camera2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var shake_amount = 1
+var shake_duration = 0.1
+var current_shake = 0
+
 func _process(delta):
 	position = get_local_mouse_position() / 4
+	if current_shake > 0:
+		shake(delta, shake_amount)
+		current_shake -= 1 * delta
+	
+func shake(delta, amount):
+	offset = Vector2(range(-1.0, 1.0).pick_random() * amount, range(-1.0, 1.0).pick_random() * amount)
