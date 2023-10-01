736d6e16bacac5719f546211abba64c2e0205b7f
steiner2001 <steiner.akos.2001@gmail.com>
2023-10-01 18:12:10 +0200

    [LD-54] fix: scene manager

--- global.gd
+++ global.gd
@@ -3,3 +3,4 @@ extends Node
 
 var last_ending = "NULL"
 var score = 0
+var is_tutorial = false
--- main_menu.gd
+++ main_menu.gd
@@ -3,6 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Global.is_tutorial = false
 	if Global.last_ending == "NULL":
 		$EndPanel.visible = false
 	elif Global.last_ending == "WIN":
