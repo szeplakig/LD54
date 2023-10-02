c14867633732147c258040c1745de5dd57bd0e75
Gergely Széplaki <szeplakig91@gmail.com>
2023-10-02 19:11:10 +0200

    GUI fixes

--- main_menu.gd
+++ main_menu.gd
@@ -4,15 +4,15 @@ extends Node
 func _ready():
 	Global.is_tutorial = false
 
-	$MarginContainer/EndPanel/Credits.visible = false
-	$MarginContainer/EndPanel/CreditsContent.visible = false
-	$MarginContainer/EndPanel/YouWon.visible = false
-	$MarginContainer/EndPanel/GameOver.visible = false
+	$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/Credits.visible = false
+	$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/CreditsContent.visible = false
+	$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/YouWon.visible = false
+	$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/GameOver.visible = false
 
 	if Global.last_ending == "NULL":
-		$MarginContainer/EndPanel/Credits.visible = true
-		$MarginContainer/EndPanel/CreditsContent.visible = true
+		$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/Credits.visible = true
+		$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/CreditsContent.visible = true
 	elif Global.last_ending == "WIN":
-		$MarginContainer/EndPanel/YouWon.visible = true
+		$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/YouWon.visible = true
 	elif Global.last_ending == "LOSE":
-		$MarginContainer/EndPanel/GameOver.visible = true
+		$MarginContainer2/VBoxContainer/MarginContainer2/EndPanel/GameOver.visible = true
