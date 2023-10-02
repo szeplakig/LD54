9e92650904d508fa88140d30d3d6ab29007bf957
steiner2001 <steiner.akos.2001@gmail.com>
2023-10-02 18:00:54 +0200

    [LD-54] fix: minor fies

--- SettingSailPanel.gd
+++ SettingSailPanel.gd
@@ -3,7 +3,7 @@ extends Panel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if get_node("../ShipFrame").phase == 8:
+	if get_node("../ShipFrame").phase == 8 and get_node("../Chest").status == "OPENED" and not get_node("../Chest").in_motion:
 		visible = true
 
 func _ready():
